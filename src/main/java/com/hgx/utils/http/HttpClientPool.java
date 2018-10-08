c718bbfbf4633edb5ef9e5509d896d9bce0fa69e
hgx <hgxappleus@gmail.com>
2018-10-08 14:34:01 +0800

    首次提交

---
+++ src/main/java/com/hgx/utils/http/HttpClientPool.java
@@ -0,0 +1,82 @@
+package com.hgx.utils.http;
+
+/*import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;*/
+
+/**
+ * 	连接管理器
+ * 
+ *
+ */
+public class HttpClientPool {
+	/**
+	 * 各配置的默认值
+	 * 
+	 * Builder() { 
+		 * super(); 
+		 * this.staleConnectionCheckEnabled = false; 
+		 * this.redirectsEnabled = true; 
+		 * this.maxRedirects = 50; 
+		 * this.relativeRedirectsAllowed = true; 
+		 * this.authenticationEnabled = true;
+		 * this.connectionRequestTimeout = -1; 
+		 * this.connectTimeout = -1; 
+		 * this.socketTimeout = -1; 
+		 * this.contentCompressionEnabled = true; 
+	 * }
+	 *
+	 *
+	 */
+	private HttpClientPool() {
+		
+	}
+	/**
+	 * 连接池的最大连接数
+	 */
+	private static final int POOL_MAX_TOTAL = 300;
+
+	/**
+	 * 设置每一个路由的最大连接数
+	 */
+	private static final int POOL_MAX_PRE_ROUTE = 50;
+	/**
+	 * 连接超时时间，客户端请求服务器与服务器建立连接（三次握手）成功的最大接受时间
+	 */
+	private static final int CONNECT_TIMEOUT = -1;
+
+	/**
+	 * 请求获取数据的超时时间,访问一个接口指定时间内无法返回数据,直接放弃此次调用
+	 */
+	private static final int SOCKET_TIMEOUT = -1;
+
+	/**
+	 * 从连接池中获取可用连接的时间
+	 */
+	private static final int CONNECTION_REQUEST_TIMEOUT = -1;
+	/**
+	 * 
+	 */
+//	private static RequestConfig requestConfig=null;
+	/**
+	 * 
+	 */
+//	private static PoolingHttpClientConnectionManager connectionManager = null;
+	static {
+		/*connectionManager = new PoolingHttpClientConnectionManager();
+		connectionManager.setMaxTotal(POOL_MAX_TOTAL);
+		connectionManager.setDefaultMaxPerRoute(POOL_MAX_PRE_ROUTE);
+		// 设置HttpClient请求超时配置
+		requestConfig = RequestConfig.custom().setConnectTimeout(CONNECT_TIMEOUT).setSocketTimeout(SOCKET_TIMEOUT).setConnectionRequestTimeout(CONNECTION_REQUEST_TIMEOUT).build();
+		*/
+	}
+	/**获取HttpClient
+	 * @return HttpClient
+	 */
+	/*public static CloseableHttpClient getHttpClient(){
+		// 获取HttpClient
+		return HttpClients.custom().setDefaultRequestConfig(requestConfig).setConnectionManager(connectionManager).build();
+	}*/
+
+}
---
+++ src/test/java/com/hgx/test/_Test.java
@@ -0,0 +1,100 @@
+package com.hgx.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipInputStream;
+
+//import org.apache.commons.lang3.StringUtils;
+//import org.apache.http.Header;
+//import org.apache.http.HttpEntity;
+//import org.apache.http.HttpResponse;
+//import org.apache.http.HttpStatus;
+//import org.apache.http.client.HttpClient;
+//import org.apache.http.client.methods.HttpPost;
+//import org.apache.http.client.utils.HttpClientUtils;
+//import org.apache.http.client.utils.URIBuilder;
+//import org.apache.http.entity.BufferedHttpEntity;
+//import org.apache.http.entity.StringEntity;
+//import org.apache.http.util.EntityUtils;
+//import org.junit.Test;
+
+//import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hgx.utils.http.HttpClientPool;
+
+
+public class _Test {
+	
+/*	@Test
+	public void post() throws IOException, URISyntaxException {
+		HttpClient httpClient=HttpClientPool.getHttpClient();
+		Map<String, Object> map = new HashMap<String, Object>();
+		ObjectMapper objectMapper = new ObjectMapper();
+		String userMapJson = objectMapper.writeValueAsString(map);
+
+		// 参数拼接，url为请求接口地址
+		URIBuilder builder = new URIBuilder("http://www.baidu.com");
+//		builder.setPath("/index");
+		HttpPost post = new HttpPost(builder.build());
+		// 构建消息实体
+		StringEntity entity = new StringEntity(userMapJson, Charset.forName("UTF-8"));
+		entity.setContentEncoding("UTF-8");
+		// 发送Json格式的数据请求
+		entity.setContentType("application/json");
+		post.setEntity(entity);
+		HttpResponse response = httpClient.execute(post);
+		// 状态码
+		int statusCode = response.getStatusLine().getStatusCode();
+		if (statusCode == HttpStatus.SC_OK) {
+			// 从response里获取数据实体并放到实体包装器中
+			HttpEntity httpEntity = response.getEntity();
+			if (httpEntity == null) {
+				System.out.println("httpEntity 为 NULL ！");
+				return;
+			}
+			BufferedHttpEntity bufferedHttpEntity = new BufferedHttpEntity(httpEntity);
+			Header header = bufferedHttpEntity.getContentType();
+			if (StringUtils.contains(header.getValue(), "application/json")) {
+				// 返回json字符串
+				String body = EntityUtils.toString(bufferedHttpEntity, "UTF-8");
+				
+				System.out.println(body);
+			} else if (StringUtils.contains(header.getValue(), "text/plain")) {
+				// 返回压缩包
+				// 获取数据流
+				InputStream in = bufferedHttpEntity.getContent();
+				// 封装成zip输入流
+				ZipInputStream zin = new ZipInputStream(in, Charset.forName("UTF-8"));
+				
+				// 文件存放地址
+				String path = "D:\\";
+				File file = null;
+				if(in!=null) {
+					in.close();
+				}
+			} else {
+				System.out.println("返回了其他类型：" + header.getValue());
+			}
+		} else {
+			System.out.println("请求失败，返回状态码为：" + statusCode);
+		}
+
+//	关闭过期连接	httpClient.getConnectionManager().closeExpiredConnections();
+		
+		 * if(response!=null) { response.close(); }
+		 
+		
+		 * HttpGet request = new HttpGet("http://localhost:8080/tao-manager-web/get/" + "啊啊啊"); request.setHeader("Accept", "application/json"); HttpResponse response = httpClient.execute(request);
+		 
+//		终止
+		if(null!=post){
+			post.abort();
+		}
+
+	}*/
+
+}
